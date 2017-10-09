95e49b6daa34887e9303bd3181d10a3b1fa2df3a
kaymatrix <kaymatrix@gmial.com>
2017-10-09 06:23:17 +0530

    templateUpdate

--- templateGUIScript.py
+++ templateGUIScript.py
@@ -9,34 +9,34 @@ import kmxQtCommonTools
 import myClass
 
 class myClassCls(QtWidgets.QMainWindow):
-	
-	def __init__(self,parent):
-		self.tag=self.__class__.__name__.replace('Cls','').upper()
-		
-		self.sch=parent
-		self.schGUIObj=self.sch.schGUIObj
-		self.schQtApp=self.sch.schQtApp
-		self.ttls=self.sch.ttls
-		self.cmttls=kmxQtCommonTools.CommonTools()
+    
+    def __init__(self,parent):
+        self.tag=self.__class__.__name__.replace('Cls','').upper()
+        
+        self.sch=parent
+        self.schGUIObj=self.sch.schGUIObj
+        self.schQtApp=self.sch.schQtApp
+        self.ttls=self.sch.ttls
+        self.cmttls=kmxQtCommonTools.CommonTools()
 
-		QtWidgets.QMainWindow.__init__(self)		
-		self.uiFile=myClass.__file__.replace(".py",".ui")
+        QtWidgets.QMainWindow.__init__(self)        
+        self.uiFile=myClass.__file__.replace(".py",".ui")
 
-		loadUi(self.uiFile, self)
-		self.setWindowTitle(self.__class__.__name__.replace('Cls',''))
+        loadUi(self.uiFile, self)
+        self.setWindowTitle(self.__class__.__name__.replace('Cls',''))
 
-		self.pushButton.clicked.connect(self.doRun)
-		
-	def initialize(self):
-		self.sch.display("myClassClsObj is working fine",self.tag)
+        self.pushButton.clicked.connect(self.doRun)
+        
+    def initialize(self):
+        self.sch.display("myClassClsObj is working fine",self.tag)
 
-	def doRun(self):
-		input = self.textEdit.toPlainText()
-		self.label.setText(input)
-		self.sch.display("Sample: " + input, self.tag)
+    def doRun(self):
+        input = self.textEdit.toPlainText()
+        self.label.setText(input)
+        self.sch.display("Sample: " + input, self.tag)
 
 if (__name__=="__main__"):
-	if(not hasattr(sch, 'myClassObj') or sch.devMode or sip.isdeleted(sch.myClassObj)):
-		sch.myClassObj = myClassCls(sch)
-	sch.myClassObj.show()
-	sch.myClassObj.raise_()
+    if(not hasattr(sch, 'myClassObj') or sch.devMode or sip.isdeleted(sch.myClassObj)):
+        sch.myClassObj = myClassCls(sch)
+    sch.myClassObj.show()
+    sch.myClassObj.raise_()
